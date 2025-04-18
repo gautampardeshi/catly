3d9695b040da49c675a95cb2a6c66c69d06351c4
gautampardeshi <pardeshigautam98@gmail.com>
2025-04-18 14:00:43 +0530

    fifth

--- catly/settings.py
+++ catly/settings.py
@@ -27,12 +27,11 @@ DEBUG = True
 
 import os
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'catly.onrender.com']
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
-
 
 
 # Application definition
