fd53c635560c5937bd3c7524937e708335e31c70
gautampardeshi <pardeshigautam98@gmail.com>
2025-04-19 15:07:45 +0530

    ninth

--- catly/settings.py
+++ catly/settings.py
@@ -86,16 +86,10 @@ WSGI_APPLICATION = 'catly.wsgi.application'
 import dj_database_url
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME':'catly_db',
-        'USER': 'ecommerce_user',
-        'PASSWORD': 'gautam0909',
-        'HOST': 'localhost',
-        'PORT': '5432',
-        'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'),conn_max_age=600,)
-
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+    )
 }
 
 
