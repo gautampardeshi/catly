5493874b13a6ed94e8eef1b6991554cb547ac7a1
gautampardeshi <pardeshigautam98@gmail.com>
2025-05-01 14:03:14 +0530

    twentyone

--- user/models.py
+++ user/models.py
@@ -25,7 +25,7 @@ class Product(models.Model):
         return self.name
 
 class Cart(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,null=True, blank=True)
     session_key = models.CharField(max_length=40, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
